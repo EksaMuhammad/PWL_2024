c7057509be92f0da5e193f22437b022e2c09684a
EksaMuhammad <144184459+EksaMuhammad@users.noreply.github.com>
2025-02-17 15:03:30 +0700

    Route langkah praktikum

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,86 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/hello', function () {
+    return "Hello";
 });
+
+Route::get('/world', function () {
+    return "World";
+});
+
+Route::get('/', function () {
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "NIM: 2341760021 - Nama: Muhammad Eksa";
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' .$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($post, $comment) {
+    return 'Pos ke-' . $post . " Komentar ke-: " . $comment;
+});
+
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+Route::get(
+    '/user/profile',
+    [UserProfileController::class, 'show']
+)->name('profile');
+
+// Generating URLs...
+$url = route('profile');
+
+// Generating Redirects...
+return redirect()->route('profile');
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+
+Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::middleware('auth')->group(function () {
+	Route::get('/user', [UserController::class, 'index']);
+	Route::get('/post', [PostController::class, 'index']);
+	Route::get('/event', [EventController::class, 'index']);
+	
+});
+
+Route::prefix('admin')->group(function () {
+	Route::get('/user', [UserController::class, 'index']);
+	Route::get('/post', [PostController::class, 'index']);
+	Route::get('/event', [EventController::class, 'index']);
+
+});
+
+Route::redirect('/here', '/there');
+
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
